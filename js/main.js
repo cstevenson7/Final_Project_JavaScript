8ca148414d8f1fdf957dbbd400834f94122b0425
cstevenson7 <cindy.stevenson1@gmail.com>
2020-09-09 20:41:52 -0600

    sorting the object

--- js/main.js
+++ js/main.js
@@ -1,107 +1,126 @@
-function getJson(){
-    //get the city form form user input
-    let city = document.querySelector("#city").value;
-    //link to the api with the city in the url
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=API_KEY_HERE`)
-    //callback function with promise
-    .then(response => response.json())
-    .then(rawData => {
-        //to confirm we are pulling data form the API
-        console.log(rawData)
-        //pulling the daily high from the json file
-        let high = rawData.main.temp_max
-        //next 2 lines adding the daily high to the html to be
-        //displayed in the element with the #high-temp id
-        let high_text = document.querySelector('#high-temp')
-        let high_f =  `${high} F`
-        high_text.innerHTML= high_f
-
-        let low = rawData.main.temp_min 
-        let low_text = document.querySelector('#low-temp')
-        let low_f =  `${low} F`
-        low_text.innerHTML= low_f
-
-        let forecast = rawData.weather[0].main
-        let forecast_text = document.querySelector('#forecast')        
-        forecast_text.innerHTML= forecast
-
-        let humidity = rawData.main.temp_min 
-        let humidity_text = document.querySelector('#humidity')
-        let humidity_per =  `${humidity} %`        
-        humidity_text.innerHTML= humidity_per        
-    })
-
-}
 //https://samples.openweathermap.org/data/2.5/weather?lat=51&lon=-114&appid=API_KEY_HERE
 //           api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 let ab_map = new Map(Object.entries({
     Calgary:[51.05,-114.09],
     Edmonton:[53.55,-113.49],
-    Radium:[50.62,-116.07],
-    Saskatoon:[52.16,-106.67],
     Banff:[51.18,-115.57],
     Nordegg:[52.47,-116.08],
-    Jasper:[52.57,-118.08],
+    Jasper:[52.57,-118.08]
+}));
+
+let bc_map = new Map(Object.entries({
+    Radium:[50.62,-116.07],
     Cranbrook:[49.51,-115.77],
     Fernie:[49.50,-115.06],
-    Regina:[50.45,-104.62],
     Kamlooops:[50.67,-120.33],
     Kelowna:[48.89, -119.50]
 }));
 
+
+function abRedirect(){
+    //window.location.href= 'ab.html';
+    let map_object = ab_map;
+    highs = new Map()
+
+    for(let[city,coordinates] of map_object.entries()){
+            // console.log(`City is ${city} and coordinates are ${coordinates}`)
+            //console.log(`Lat is  ${coordinates[0]} and lon is ${coordinates[1]}`)
+            //console.log(`http://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&units=imperial&appid=API_KEY_HERE)`)
+            //link to the api with the lat & lon in the url
+                   http://api.openweathermap.org/data/2.5/weather?lat=51&lon=-114&units=imperial&appid=72fed5dd48ff06dcf670a1689cf0dc55
+            fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&units=metric&appid=API_CODE_HERE`)
+            //callback function with promise
+            .then(response => response.json())
+            .then(rawData => {                                      
+                //pulling the current temp from the json file
+                let current = rawData.main.temp_max
+                let description = rawData.weather[0].description
+                //Getting the city from my map
+                let city_name = city
+                //console.log(rawData.main.temp)   
+                highs.set(city,[current,description])
+                //console.log(highs)       
+             })
+    }
+    return highs
+}
+
+function getTop4(highs){
+    if(highs.size < 1){
+        // middle index  should be three for albaeta
+        mid = Math.floor(high.size)
+        console.log(mid)
+
+
+    }
+ 
+    console.log('getTop4')
+
+}
+
+
+function bcRedirect(){
+    window.location.href= 'bc.html';
+}
                                 
-function getJson1(){
-    //get the Lat & lon  from  user input
-    let prov = document.querySelector("#prov").value;
+function getJson1(){   
+    //let prov = then value of the button selected
     if(prov === 'AB'){
         map_object = ab_map
     }else{
         map_object = bc_map
     }
-    map_object = ab_map
+
+    let map_object = ab_map;
+    highs = new Map()
+
     for(let[city,coordinates] of map_object.entries()){
-        console.log(`City is ${city} and coordinates are ${coordinates}`)
-        console.log(`Lat is  ${coordinates[0]} and lon is ${coordinates[1]}`)
-
-    //link to the api with the lat & lon in the url
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=API_KEY_HERE`)
-    //callback function with promise
-    .then(response => response.json())
-    .then(rawData => {
-        //to confirm we are pulling data form the API
-        console.log(rawData)
-        
-        //pulling the daily high from the json file
-        let high = rawData.main.temp_max
-        //next 2 lines adding the daily high to the html to be
-        //displayed in the element with the #high-temp id
-        let high_text = document.querySelector('#high-temp')
-        let high_f =  `${high} F`
-        high_text.innerHTML= high_f
-
-        let low = rawData.main.temp_min 
-        let low_text = document.querySelector('#low-temp')
-        let low_f =  `${low} F`
-        low_text.innerHTML= low_f
-
-        let forecast = rawData.weather[0].main
-        let forecast_text = document.querySelector('#forecast')        
-        forecast_text.innerHTML= forecast
-
-        let humidity = rawData.main.temp_min 
-        let humidity_text = document.querySelector('#humidity')
-        let humidity_per =  `${humidity} %`        
-        humidity_text.innerHTML= humidity_per        
-    })
-
+            console.log(`City is ${city} and coordinates are ${coordinates}`)
+            //console.log(`Lat is  ${coordinates[0]} and lon is ${coordinates[1]}`)
+            //console.log(`http://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&units=imperial&appid=API_KEY_HERE)`)
+            //link to the api with the lat & lon in the url
+                   http://api.openweathermap.org/data/2.5/weather?lat=51&lon=-114&units=imperial&appid=72fed5dd48ff06dcf670a1689cf0dc55
+            fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&units=metric&appid=72fed5dd48ff06dcf670a1689cf0dc55`)
+            //callback function with promise
+            .then(response => response.json())
+            .then(rawData => {                                      
+                //pulling the current temp from the json file
+                let current = rawData.main.temp_max
+                let description = weather[0].description
+                //Getting the city from my map
+                let city_name = city
+                console.log(rawData.main.temp)   
+                highs.set(city,[current,description])       
+             })
     }
 }
-function abRedirect(){
-    window.location.href= 'ab.html';
-}
 
-function bcRedirect(){
-    window.location.href= 'bc.html';
+
+function displayTop4(highs){
+        
+            //pulling the daily high from the json file
+            let high = rawData.main.temp_max
+            //next 2 lines adding the daily high to the html to be
+            //displayed in the element with the #high-temp id
+            let high_text = document.querySelector('#high-temp')
+            let high_f =  `${high} F`
+            high_text.innerHTML= high_f
+
+            let low = rawData.main.temp_min 
+            let low_text = document.querySelector('#low-temp')
+            let low_f =  `${low} F`
+            low_text.innerHTML= low_f
+
+            let forecast = rawData.weather[0].main
+            let forecast_text = document.querySelector('#forecast')        
+            forecast_text.innerHTML= forecast
+
+            let humidity = rawData.main.temp_min 
+            let humidity_text = document.querySelector('#humidity')
+            let humidity_per =  `${humidity} %`        
+            humidity_text.innerHTML= humidity_per  
+
+
 }
 
 function clearData(){
